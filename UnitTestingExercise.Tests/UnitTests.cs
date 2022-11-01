511238fa2d9cf889932d6e2d3e57801131ed43bf
Fahad Janabi <Fahad.janabi98@yahoo.com>
2022-11-01 15:35:18 -0400

    UnitTestingExercise

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
+        private IEnumerable<object> actual;
+
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
         public void AddTest(int num1, int num2, int num3, int expected)
@@ -12,56 +15,59 @@ namespace UnitTestingExercise.Tests
             //Start Step 3 here:
 
             //Arrange
-            // create a Calculator object
-            
+            var test = new Calculator();
 
             //Act
-                // call the Add method that is located in the Calculator class
-                // and store its result in a variable named actual
+            // call the Add method that is located in the Calculator class
+            // and store its result in a variable named actual
+            var actual = test.Add(num1, num2, num3);
 
             //Assert
                 //Assert.Equal(expected, actual);
-        }
+                Assert.Equal(expected, actual);
+        }     
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(0, 0, 0)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
             //Arrange
+            var tester = new Calculator();
 
             //Act
+            var actual =tester.Subtract(minuend, subtrhend);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(2, 2, 4)]//Add test data <-------
+        [InlineData(5, 4, 20)]
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
 
             //Arrange
-
+            var test =new Calculator();
             //Act
-
+           var actual= test.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(20, 5, 4)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
-
+            var tester = new Calculator();
             //Act
-
+            var actual =tester.divide(num1, num2); 
             //Assert
-
+            Assert.Equal(expected,actual);
         }
-
     }
 }
--- UnitTestingExercise/Calculator.cs
+++ UnitTestingExercise/Calculator.cs
@@ -8,14 +8,33 @@ namespace UnitTestingExercise
         // Right-click on the 'Dependencies' folder in your UnitTestingExercise.Tests project
         // and add your reference to the UnitTestingExercise project
 
+        
+        public int Add(int one, int two, int three)
+        {
+            return one + two + three;
+        }
 
+        public int Subtract(int minuend, int subtrend)
+        {
+            return minuend - subtrend;
+        }
+
+        public int Multiply(int one, int two)
+        {
+            return one * two;
+        }
+
+        public int divide(int one, int two)
+        {
+            return two / one;
+        }
 
         // Step 2: Create an Add method that accepts 3 integers, adds those integers, and returns an integer
         //For Example:
-        public int Add(int num1, int num2, int num3)
-        {
-            return num1 + num2 + num3;
-        }
+        // public int Add(int num1, int num2, int num3)
+        //{
+        //return num1 + num2 + num3;
+        //}
 
 
         // Step 3: Navigate to the UnitTests.cs file and complete the AddTest unit test method 
